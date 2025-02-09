1e20b0adec813801ba7057cdd5efe50abe8b2fa7
Gavrikov Nikita <nikitagav2004@gmail.com>
2025-02-09 20:50:46 +0300

    urls and views

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev'
+]
